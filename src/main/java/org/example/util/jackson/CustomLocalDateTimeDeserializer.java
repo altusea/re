e4f2f38376110910d39146a46af9ef266f0ae1f8
altusea <zt.song@outlook.com>
2024-02-20 20:54:48 +0800

    add

--- src/main/java/org/example/util/jackson/CustomLocalDateTimeDeserializer.java
+++ src/main/java/org/example/util/jackson/CustomLocalDateTimeDeserializer.java
@@ -2,6 +2,7 @@ package org.example.util.jackson;
 
 import cn.hutool.core.date.DatePattern;
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import org.apache.commons.lang3.StringUtils;
@@ -20,20 +21,26 @@ public class CustomLocalDateTimeDeserializer extends LocalDateTimeDeserializer {
 
     @Override
     public LocalDateTime deserialize(JsonParser parser, DeserializationContext context) throws IOException {
-        String text = parser.getText();
-        if (StringUtils.isEmpty(text)) {
-            return null;
-        }
-        if (text.length() == 13) {
-            // 时间戳
-            return LocalDateTime.ofInstant(Instant.ofEpochMilli(Long.parseLong(text)), ZoneId.systemDefault());
-        } else if (text.length() == DatePattern.NORM_DATE_PATTERN.length()) {
-            // "yyyy-MM-dd"
-            return LocalDate.parse(text, DatePattern.NORM_DATE_FORMATTER).atStartOfDay();
-        } else if (text.indexOf('T') >= 0) {
-            return super.deserialize(parser, context);
-        } else if (text.length() == DatePattern.NORM_DATETIME_PATTERN.length()) {
-            return LocalDateTime.parse(text, DatePattern.NORM_DATETIME_FORMATTER);
+        if (parser.hasToken(JsonToken.VALUE_NUMBER_INT)) {
+            long timestamp = parser.getLongValue();
+            // Handle timestamp conversion to LocalDateTime
+            return LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.systemDefault());
+        } else if (parser.hasToken(JsonToken.VALUE_STRING)) {
+            String text = parser.getText();
+            if (StringUtils.isEmpty(text)) {
+                return null;
+            }
+            if (text.length() == 13) {
+                // 时间戳
+                return LocalDateTime.ofInstant(Instant.ofEpochMilli(Long.parseLong(text)), ZoneId.systemDefault());
+            } else if (text.length() == DatePattern.NORM_DATE_PATTERN.length()) {
+                // "yyyy-MM-dd"
+                return LocalDate.parse(text, DatePattern.NORM_DATE_FORMATTER).atStartOfDay();
+            } else if (text.indexOf('T') >= 0) {
+                return super.deserialize(parser, context);
+            } else if (text.length() == DatePattern.NORM_DATETIME_PATTERN.length()) {
+                return LocalDateTime.parse(text, DatePattern.NORM_DATETIME_FORMATTER);
+            }
         }
         return super.deserialize(parser, context);
     }
