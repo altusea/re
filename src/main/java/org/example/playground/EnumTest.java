19ae59d8d14e663ea5dac9a0e6a8a7306ba4c6d7
altusea <zt.song@outlook.com>
2024-04-27 16:10:24 +0800

    add more examples

--- src/main/java/org/example/playground/EnumTest.java
+++ src/main/java/org/example/playground/EnumTest.java
@@ -2,7 +2,11 @@ package org.example.playground;
 
 import org.apache.commons.lang3.EnumUtils;
 
+import java.util.EnumSet;
 import java.util.Map;
+import java.util.Set;
+
+import static org.example.playground.CommonTest.printSeparateLine;
 
 public class EnumTest {
 
@@ -24,8 +28,42 @@ public class EnumTest {
         }
     }
 
+    enum EmployeePosition {
+        SRE,
+        ARCHITECT,
+        DEVELOPER
+    }
+
     public static void main(String[] args) {
         Map<String, Color> map = EnumUtils.getEnumMap(Color.class, Color::getCode);
         System.out.println(map.get(Color.RED.code));
+
+        // with EnumSet
+        printSeparateLine();
+        var x = EnumSet.of(
+                EmployeePosition.SRE,
+                EmployeePosition.ARCHITECT,
+                EmployeePosition.DEVELOPER);
+        long begX = System.nanoTime();
+        for (int i = 0; i < 100_000_000; i++) {
+            var es = EnumSet.allOf(EmployeePosition.class);
+            es.containsAll(x);
+        }
+        long endX = System.nanoTime();
+        System.out.println(x.getClass() + ": " + (endX - begX) / 1e9);
+
+        // with Set
+        printSeparateLine();
+        var y = Set.of(
+                EmployeePosition.SRE,
+                EmployeePosition.ARCHITECT,
+                EmployeePosition.DEVELOPER);
+        long begY = System.nanoTime();
+        for (int i = 0; i < 100_000_000; i++) {
+            var hs = Set.of(EmployeePosition.values());
+            hs.containsAll(y);
+        }
+        long endY = System.nanoTime();
+        System.out.println(y.getClass() + ": " + (endY - begY) / 1e9);
     }
 }
---
+++ src/test/java/org/example/playground/StreamTest.java
@@ -0,0 +1,68 @@
+package org.example.playground;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class StreamTest {
+
+    @Test
+    public void test1() {
+        // unmodifiable list
+        var l = Stream.of(null, "Green", "Yellow").toList();
+        assertEquals(3, l.size());
+        assertThrows(UnsupportedOperationException.class, () -> l.add("Red"));
+        assertThrows(UnsupportedOperationException.class, () -> l.set(0, "Red"));
+    }
+
+    @Test
+    public void test2() {
+        assertThrows(NullPointerException.class, () ->
+                Stream.of(null, "Green", "Yellow").collect(Collectors.toUnmodifiableList()));
+    }
+
+    record Employee(String firstName,
+                           String lastName,
+                           String position,
+                           int salary) {
+    }
+
+    @Test
+    public void test3() {
+        Stream<Employee> s1 = Stream.of(
+                new Employee("AAA", "BBB", "developer", 10000),
+                new Employee("AAB", "BBC", "architect", 15000),
+                new Employee("AAC", "BBD", "developer", 13000),
+                new Employee("AAD", "BBE", "tester", 7000),
+                new Employee("AAE", "BBF", "tester", 9000)
+        );
+
+        var m = s1.collect(Collectors.groupingBy(Employee::position,
+                Collectors.summingInt(Employee::salary)));
+        assertEquals(3, m.size());
+        assertEquals(m.get("developer"), 23000);
+        assertEquals(m.get("architect"), 15000);
+        assertEquals(m.get("tester"), 16000);
+    }
+
+    @Test
+    public void test4() {
+        Stream<Employee> s1 = Stream.of(
+                new Employee("AAA", "BBB", "developer", 10000),
+                new Employee("AAB", "BBC", "architect", 15000),
+                new Employee("AAC", "BBD", "developer", 13000),
+                new Employee("AAD", "BBE", "tester", 7000),
+                new Employee("AAE", "BBF", "tester", 9000)
+        );
+
+        var m = s1.collect(Collectors.partitioningBy(emp -> emp.salary() > 10000));
+        assertEquals(2, m.size());
+        assertEquals(m.get(true).size(), 2);
+        assertEquals(m.get(false).size(), 3);
+    }
+
+}
