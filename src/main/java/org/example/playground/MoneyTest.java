10224a6cdbeac8fee8a75e225f25426814da2865
altusea <zt.song@outlook.com>
2024-07-11 08:17:54 +0800

    update pom.xml

---
+++ src/main/java/org/example/playground/MoneyTest.java
@@ -0,0 +1,14 @@
+package org.example.playground;
+
+import org.joda.money.CurrencyUnit;
+import org.joda.money.Money;
+
+public class MoneyTest {
+
+    public static void main(String[] args) {
+        CurrencyUnit currencyUnit = CurrencyUnit.of("CNY");
+        Money money = Money.of(currencyUnit, 1000000000000.00);
+        var b = money.getAmount();
+        System.out.println(b.toPlainString());
+    }
+}
